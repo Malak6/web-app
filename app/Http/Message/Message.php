aa1faf76491a13edde9892c969402e998f8675e1
Malak <malaksrhan914@gmail.com>
2023-11-21 21:31:53 +0200

    update message

--- app/Http/Message/Message.php
+++ app/Http/Message/Message.php
@@ -1,38 +1,21 @@
 <?php
-
 namespace App\Http\Message;
-
 class Message {
-    //Create the sigle instance
-    private static $instance = null;
     //Create the msg property
     private $msg = [];
 
-    //Make the constructor private so that this class cannot be instantiated
-    private function __construct(){
+    public function __construct(){
         $this->msg =
-            [
-                "key1" => 5 ,
-                "key2" => "value1" ,
-                "key3" => "value1" ,
-            ];
-    }
-    //Get the only instance available
-    public static function getInstance(){
-        if (self::$instance == null) {
-            self::$instance = new Message();
-        }
-        return self::$instance;
+            [ "key1" => 5 , "key2" => "value1" ,"key3" => "value1" ];
     }
     //Get the msg property 
     public function getMessage(){
         return $this->msg ;
     }
-
-
-// public static function setMessage($){
-//     return $msg;
-// }
-
-    
+    // update to handle array of keys
+    public function setMessage($key,$value){
+        if(!$this->msg[$key])  return "no such property";
+        $this->msg[$key] = $value;
+        return $this->msg;
+    }
 }
