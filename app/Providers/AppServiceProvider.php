571c0e732593628d643b7b625994b6538758ab30
Malak Srhan <82179938+Malak6@users.noreply.github.com>
2023-11-25 22:50:58 +0300

    roll back facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Go\Core\AspectKernel;
-use Go\Core\AspectContainer;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -16,19 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-    $this->app->singleton(AspectKernel::class, function () {
-        return AspectKernel::getInstance();
-    });
-
-    $this->app->singleton(AspectContainer::class, function () {
-        /*
-        $container = $this->app->make(AspectKernel::class)->getContainer();
-        $container->registerAspect(LoggingAspect::class);
-        $container->registerAspect(MessageAspect::class); // تسجيل الجانب MessageAspect
-
-        return $container;*/
-    });
+        // 
     }
 
     /**
